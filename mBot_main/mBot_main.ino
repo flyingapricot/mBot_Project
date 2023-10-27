be66ace083eaa8306da023223a35f666d27d5c7b
kinneo <142193380+kinneo@users.noreply.github.com>
2023-10-27 10:59:21 +0800

    Update mBot_main.ino

--- mBot_main/mBot_main.ino
+++ mBot_main/mBot_main.ino
@@ -26,50 +26,95 @@ void stopRobot() {
 // void moveForward() {// Code for moving forward for some short interval}
 
 // green - turning right 90 deg
-// void turnRight() {
+void turnRight() {
 // spin left motor forwards and right motor backwards for t
+ digitalWrite(leftMotorPin1, HIGH);
+  digitalWrite(leftMotorPin2, LOW);
+  digitalWrite(rightMotorPin1, LOW);
+  digitalWrite(rightMotorPin2, HIGH);
+   delay(1500);
 // stop both left and right motor for stabilising b4 next action 
   leftMotor.stop();
   rightMotor.stop();
-//}
+}
 
 // red - turning left 90 deg
-// void turnLeft() {
+void turnLeft() {
 // spin left motor backwards and right motor forwards for t
+ digitalWrite(leftMotorPin1, LOW);
+  digitalWrite(leftMotorPin2, HIGH);
+  digitalWrite(rightMotorPin1, HIGH);
+  digitalWrite(rightMotorPin2, LOW);
+delay(1500);
 // stop both left and right motor for stabilising b4 next action 
   leftMotor.stop();
   rightMotor.stop();
-// }
+}
 
 // orange - uturn
-// void uTurn() {
+void uTurn() {
 // spin left motor backwards and right motor forwards for 2t
+ digitalWrite(leftMotorPin1, LOW);
+  digitalWrite(leftMotorPin2, HIGH);
+  digitalWrite(rightMotorPin1, HIGH);
+  digitalWrite(rightMotorPin2, LOW);
+delay(3000);
 // stop both left and right motor for stabilising b4 next action 
   leftMotor.stop();
   rightMotor.stop();
-// }
+}
 
 // purple - double left turn
-// void doubleLeftTurn() {
-// spin left motor backwards and right motor forwards for t
+void doubleLeftTurn() {
+// spin left motor backwards and right motor forwards for t 
+digitalWrite(leftMotorPin1, LOW);
+  digitalWrite(leftMotorPin2, HIGH);
+  digitalWrite(rightMotorPin1, HIGH);
+  digitalWrite(rightMotorPin2, LOW);
+delay(1500);
 // stop both left and right motor for stabilising b4 next action
   leftMotor.stop();
   rightMotor.stop();
 // both motor moves forward for t
+digitalWrite(leftMotorPin1, HIGH);
+  digitalWrite(leftMotorPin2, LOW);
+  digitalWrite(rightMotorPin1, HIGH);
+  digitalWrite(rightMotorPin2, LOW);
+delay(2000);
 // spin left motor backwards and right motor forwards for t
+ digitalWrite(leftMotorPin1, LOW);
+  digitalWrite(leftMotorPin2, HIGH);
+  digitalWrite(rightMotorPin1, HIGH);
+  digitalWrite(rightMotorPin2, LOW);
+delay(1500);
 // stop both left and right motor for stabilising b4 next action
   leftMotor.stop();
   rightMotor.stop();
-//}
+}
 
 // blue
-// void doubleRightTurn() {
+void doubleRightTurn() {
 // spin left motor backwards and right motor forwards for t
+ digitalWrite(leftMotorPin1, HIGH);
+  digitalWrite(leftMotorPin2, LOW);
+  digitalWrite(rightMotorPin1, LOW);
+  digitalWrite(rightMotorPin2, HIGH);
+   delay(1500);
 // stop both left and right motor for stabilising b4 next action
   leftMotor.stop();
   rightMotor.stop();
 // both motor moves forward for t
+digitalWrite(leftMotorPin1, HIGH);
+  digitalWrite(leftMotorPin2, LOW);
+  digitalWrite(rightMotorPin1, HIGH);
+  digitalWrite(rightMotorPin2, LOW);
+delay(2000);
 // spin left motor backwards and right motor forwards for t
+ digitalWrite(leftMotorPin1, HIGH);
+  digitalWrite(leftMotorPin2, LOW);
+  digitalWrite(rightMotorPin1, LOW);
+  digitalWrite(rightMotorPin2, HIGH);
+   delay(1500);
 // stop both left and right motor for stabilising b4 next action 
   leftMotor.stop();
   rightMotor.stop();
