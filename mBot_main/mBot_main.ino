dfc153441ed06ad3bfc8e0e77aa4da7e881c43f2
flyingapricot <neerajkumbar44@gmail.com>
2023-11-06 22:24:43 +0800

    Removed duplicate functions and definitions

--- mBot_main/mBot_main.ino
+++ mBot_main/mBot_main.ino
@@ -175,6 +175,14 @@ MeDCMotor rightMotor(M2); // assigning RightMotor to port M2
 MeLineFollower lineFollower(PORT_2); // assigning lineFollower to RJ25 port 2
 MeBuzzer buzzer; // create the buzzer object
 
+//By varying the analog output of A and B (At Port 4), we can control each of the LEDs + IR Emitter
+#define A A0 //S1 (Pin 2 of Decoder)
+#define B A1 //S2 (Pin 3 of Decoder)
+#define LDR A2 //Using Port 3 S1 to read LDR Voltage
+#define IRD A3 //Using Port 3 S2 to read IR Detector Voltage
+
+
+
 int status = 0; // global status; 0 = do nothing, 1 = mBot runs
 
 //High Low Pair used to specifically control each R, G and B LED by varying A and B's Analog Signal
@@ -182,19 +190,6 @@ struct LEDPair {
   uint8_t A_val;
   uint8_t B_val;
 };
-
-
-int shineIR() {
-  //Power on IR Emitter
-  analogWrite(A,LOW); //Setting A0 to High/Low
-  analogWrite(B,LOW); //Setting A1 to High/Low
-
-  delay(500);
-
-  int ans = analogRead(IRD);
-
-  return ans;
-}
 
 
 /** Start of mBot movement functions**/
@@ -289,12 +284,6 @@ void successiveRight() {
 #define RGBWait 200 //in milliseconds
 // Define time delay before taking another LDR reading
 #define LDRWait 10 //in milliseconds
-#define LDR A2 //Using Port 3 S1 to read LDR Voltage
-#define IRD A3 //Using Port 3 S2 to read IR Detector Voltage
-
-//By varying the analog output of A and B (At Port 4), we can control each of the LEDs + IR Emitter
-#define A A0 //S1 (Pin 2 of Decoder)
-#define B A1 //S2 (Pin 3 of Decoder)
 
 //Green LED(A - H, B - L) at Y1, Blue LED at Y2(A- L, B -H) , Red LED at Y3 (A- H, B -H)
 LEDPair LED_Array[3];
@@ -312,10 +301,6 @@ float greyDiff[] = {0,0,0};
 
 /** End of LDR Related Variables **/
 
-
-
-bool do_color_decode = false;
-
 enum Colours {
   detectPurple,
   detectGreen,
@@ -324,6 +309,8 @@ enum Colours {
   detectOrange,
   detectWhite
 };
+
+bool do_color_decode = false;
 
 
 void celebrate() {
@@ -335,100 +322,6 @@ void celebrate() {
     
     buzzer.noTone();
   }
-}
-
-
-void stopRobot() {
-  leftMotor.stop();
-  rightMotor.stop();
-}
-
-// 🟢 Called when GREEN detected at waypoint.
-//Turns mBot to the right by 90 degrees (on the spot).
-// TODO : Higher Delay (1500?)
-void turnRight() {
-  leftMotor.run(-255); // Positive: wheel turns anti-clockwise
-  rightMotor.run(-255); // Positive: wheel turns anti-clockwise
-  delay(310);
-  leftMotor.stop();
-  rightMotor.stop();
-
-}
-
-// 🔴 Called when RED detected at waypoint.
-// Turns mBot to the left by 90 degrees (on the spot).
-// TODO : Higher Delay (1500?)
-void turnLeft() {
-  leftMotor.run(255); // Positive: wheel turns anti-clockwise
-  rightMotor.run(255); // Positive: wheel turns anti-clockwise
-  delay(310);
-  leftMotor.stop();
-  rightMotor.stop();
-}
-
-
-// 🟣 Called when PURPLE detected at waypoint.
-//Turns the mBot successively to the left twice
-//TODO: Higher delay at 2000ms while moving straight?
-void successiveLeft() {
-  // 1. Turn mBot to the left by 90 degrees.
-  turnLeft();
-
-  // 2. Then, move mBot forward by one tile.
-  leftMotor.run(-255);
-  rightMotor.run(255);
-  delay(700);  // TODO: EDIT VALUE BASED ON LAB VALUES. PREV VAL: 600
-
-  // 3. After moving forward, stop motors and wait for a short duration for mBot to stabilise.
-  leftMotor.stop();
-  rightMotor.stop();
-  delay(20);
-
-  // 4. Finally, turn mBot to the left by 90 degrees again.
-  turnLeft();
-}
-
-// 🔵 Called when BLUE detected at waypoint.
-//Turns the mBot successively to the left twice
-//TODO: Higher delay at 2000ms while moving straight?
-void successiveRight() {
-  // 1. Turn mBot to the right by 90 degrees.
-  turnRight();
-
-  // 2. Then, move mBot forward by one tile.
-  leftMotor.run(-255);
-  rightMotor.run(255);
-  delay(700);  // TODO: EDIT VALUE BASED ON LAB VALUES. (Last: 600)
-
-  // 3. After moving forward, stop motors and wait for a short duration for mBot to stabilise.
-  leftMotor.stop();
-  rightMotor.stop();
-  delay(20);
-
-  // 4. Finally, turn mBot to the right by 90 degrees again.
-  turnRight();
-
-}
-
-// 🟠 Called when ORANGE detected at waypoint when closer to left wall.
-//TODO: Higher Single Delay after UTurn? (3000ms)
-void uTurn() {
-  // Turn mBot 180 degrees by spinning to the left.
-  // Spin left motor backward and right motor forward to turn mBot to the left.
-
-  // leftMotor.run(100);
-  // rightMotor.run(0);
-  // delay(200);
-
-  leftMotor.run(255);
-  rightMotor.run(255);
-  delay(575);  // Keep turning until turn is 180 degrees.
-
-  // After turn, stop motors and wait for a short duration for mBot to stabilise.
-  leftMotor.stop();
-  rightMotor.stop();
-  delay(300);
-
 }
 
 // void moveForward() {// Code for moving forward for some short interval}
@@ -449,89 +342,6 @@ int shineIR() {
 
 }
 
-void shineRed() {    
-  analogWrite(A,LED_Array[0].A_val); //Setting A0 to High/Low
-  analogWrite(B,LED_Array[0].B_val); //Setting A0 to High/Low
-}
-
-void shineGreen() {
-  analogWrite(A,LED_Array[1].A_val); //Setting A0 to High/Low
-  analogWrite(B,LED_Array[1].B_val); //Setting A0 to High/Low
-}
-
-void shineBlue() {
-  analogWrite(A,LED_Array[2].A_val); //Setting A0 to High/Low
-  analogWrite(B,LED_Array[2].B_val); //Setting A0 to High/Low
-}
-
-int detectColour()
-{
-
-  // Red [0], Green [1], Blue [2]
-
-  // Shine Red, read LDR after some delay
-  shineRed();
-  delay(RGBWait);
-  colourArray[0] = getAvgReading(5);
-  colourArray[0] = (colourArray[0] - blackArray[0])/(greyDiff[0])*255;
-  delay(RGBWait);
-  Serial.println(int(colourArray[0]));
-
-  // Shine Green, read LDR after some delay
-  shineGreen();
-  delay(RGBWait);
-  colourArray[1] = getAvgReading(5);
-  colourArray[1] = (colourArray[1] - blackArray[1])/(greyDiff[1])*255;
-  delay(RGBWait);
-  Serial.println(int(colourArray[1]));
-
-  // Shine Blue, read LDR after some delay
-  shineBlue();
-  delay(RGBWait);
-  colourArray[2] = getAvgReading(5);
-  colourArray[2] = (colourArray[2] - blackArray[2])/(greyDiff[2])*255;
-  delay(RGBWait);
-  Serial.println(int(colourArray[2]));
-  analogWrite(A,LOW); //Setting A0 to High/Low
-  analogWrite(B,LOW); //Setting A0 to High/Low
-
-   // Red [0], Green [1], Blue [2]
-
-  // If R, G and B near 255 = WHITE
-  if (colourArray[0] >= 180 && colourArray[1] >= 180 && colourArray[2] >= 180) {
-    Serial.println("ITS WHITE!!!");
-    return detectWhite; // Stops
-  }
-  // When Green is MAX
-  if (colourArray[1] > colourArray[0] && colourArray[1] > colourArray[2]) {
-    double ratio = colourArray[1]/colourArray[2]; // Green/Blue ratio
-    // Using Red as differentiator
-    if (colourArray[0] >= 130) {
-      Serial.println("ITS PURPLE!!!");
-      return detectPurple; // Double Left turn
-    }
-    else if (colourArray[0] >= 95) {
-      Serial.println("ITS BLUE!!!");
-      return detectBlue; // Double Right turn
-    } 
-    else {
-      Serial.println("ITS GREEN!!!");
-      return detectGreen; // Right turn
-    }
-  }
-  // When Red is MAX
-  else if (colourArray[0] > colourArray[1] && colourArray[0] > colourArray[2]) {
-      double ratio = colourArray[1]/colourArray[0]; // Green/Red ratio
-      if (ratio < 0.9) {
-        Serial.println("ITS RED!!!");
-        return detectRed; // Left turn
-      } 
-      else {
-        Serial.println("ITS ORANGE!!!");
-        return detectOrange; // U-Turn
-      }
-  }
-}
 
 int getAvgReading(int times){
   //find the average reading for the requested number of times of scanning LDR
@@ -599,31 +409,6 @@ void setBalance() {
 
 /** End of LDR Colour Calibration**/
 
-
-bool do_color_decode = false;
-
-int getAvgReading(int times){
-  //find the average reading for the requested number of times of scanning LDR
-  int reading;
-  int total = 0;
-  //take the reading as many times as requested and add them up
-  for(int i = 0;i < times;i++){
-    reading = analogRead(LDR);
-    total = reading + total;
-    delay(LDRWait);
-  }
-  //calculate the average and return it
-  return total/times;
-}
-
-enum Colours {
-  detectWhite,
-  detectRed,
-  detectGreen,
-  detectOrange,
-  detectPurple,
-  detectBlue, 
-};
 
 
 /** Start of LDR Colour Detection**/
@@ -834,11 +619,6 @@ void loop()
 
       enum Colours colour = detectColour();
       switch(colour) {
-        case detectWhite:
-          stopRobot();
-          rickroll(); // play "Never Gonna Give You Up"
-          delay(5000);
-          break;
         case detectRed:
           Serial.println("RED DETECTED!!!!");
           turnLeft();
